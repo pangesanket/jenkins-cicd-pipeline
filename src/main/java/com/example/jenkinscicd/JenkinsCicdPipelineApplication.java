3088d3192a74b001df8df7cf8b83a48b3a99839d
amolnandiwale <amol.nandiwale@checkerslab.com>
2024-05-21 10:54:55 +0530

    first commit

---
+++ src/main/java/com/example/jenkinscicd/JenkinsCicdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinscicd/JenkinsCicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
